10453d6eef0e82ad5faa1f3c074cb3836db41e4a
SajaJawad <saiamaher2002@gmail.com>
2025-10-20 13:54:59 +0300

    Search Result

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -4,23 +4,29 @@ import { Input } from "@/components/ui/input"
 import useDebounce from "@/hooks/useDebounce";
 import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutations";
 import { Loader } from "lucide-react";
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
+import { InView, useInView } from 'react-intersection-observer'
 const Explore = () => {
-const {data: posts , fetchNextPage, hasNextPage}= useGetPosts()
+
+  const { ref ,  inView } = useInView()
+  const { data: posts, fetchNextPage, hasNextPage } = useGetPosts()
 
   const [searchValue, setSearchValue] = useState('')
 
-  const debouncedValue= useDebounce(searchValue,500);
-  const {data: searchedPosts, isFetching: isSearchFetching}= useSearchPosts(debouncedValue)
+  const debouncedValue = useDebounce(searchValue, 500);
+  const { data: searchedPosts, isFetching: isSearchFetching } = useSearchPosts(debouncedValue)
 
-if (!posts) {
-  return (
-    <div className="flex-center w-full h-full">
-      <Loader/>
-    </div>
-  )
-}  
+  useEffect(() => {
+    if (inView && !searchValue) fetchNextPage()
+  }, [inView, searchValue])
+
+  if (!posts) {
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    )
+  }
 
 
   const shouldShowSearchResults = searchValue !== '';
@@ -65,7 +71,7 @@ if (!posts) {
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
           <SearchResults
-            isSearchFetching = {isSearchFetching}
+            isSearchFetching={isSearchFetching}
             searchedPosts={searchedPosts}
           />
         ) : shouldShowPosts ? (
@@ -74,6 +80,12 @@ if (!posts) {
           <GridPostList key={`page-${index}`} posts={item.documents} />
         ))}
       </div>
+
+      {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+      )}
 
     </div>
   )
